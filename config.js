02c532c7497ed5d4f2e8d0778d95ade303b36b0a
rhnvenugopal <64375531+rhnvenugopal@users.noreply.github.com>
2023-12-03 11:23:38 -0500

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAIFn3s95Jael3XOa8SINmrotA8SsC1EOE",
+  authDomain: "pr61-9eeb2.firebaseapp.com",
+  databaseURL: "https://pr61-9eeb2-default-rtdb.firebaseio.com",
+  projectId: "pr61-9eeb2",
+  storageBucket: "pr61-9eeb2.appspot.com",
+  messagingSenderId: "949064060315",
+  appId: "1:949064060315:web:1d901b904b846cb25fa53e"
+};
